c7b5324c359c8a5d25c332392e37fa47c55d11dc
Jaime Vallejo <jaimed.vallejo@gmail.com>
2019-06-21 12:21:42 -0500

    Add Apk

--- app/src/main/java/com/jaime/marvel/models/Trailer.kt
+++ app/src/main/java/com/jaime/marvel/models/Trailer.kt
@@ -3,9 +3,9 @@ package com.jaime.marvel.models
 import java.io.Serializable
 
 data class Trailer(val id : String,
-                   val key: String,
-                   val name: String,
-                   val site: String,
-                   val size: Int,
-                   val type: String
-            ) : Serializable
+                                               val key: String,
+                                               val name: String,
+                                               val site: String,
+                                               val size: Int,
+                                               val type: String
+) : Serializable
